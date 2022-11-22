3519dc3bd9e16760763466bdf7de984ffba97173
henriquejac <50842961+henriquejac@users.noreply.github.com>
2022-11-22 20:29:31 -0300

    Add files via upload

--- Atividade13/Atividade13.js
+++ Atividade13/Atividade13.js
@@ -1,3 +1,12 @@
+function check(){
+    var radios = document.getElementsByName('caps');
+    if(radios[0].checked){
+        maiusculas();
+    }else{
+        minusculas();
+    }
+}
+
 function maiusculas(){
     document.querySelector("#texto").value = document.querySelector("#texto").value.toUpperCase() ;
 }
